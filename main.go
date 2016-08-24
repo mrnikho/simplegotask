f866652c9c9f5c11442a5c1bf66d20902feb95ef
nikhil <nikhilraman@yahoo.com>
2016-08-24 22:40:20 +0530

    Fix some formatting

--- main.go
+++ main.go
@@ -4,53 +4,50 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"time"
+	"net/http"
 	"os"
-	)
-
+	"time"
+)
 
 type ResponseList struct {
 	Responses []Response
 }
 
 //Type holds our parsed response
-type Response struct{
-	Time string `json:time`
+type Response struct {
+	Time string      `json:time`
 	Data []FieldInfo `json:data`
 }
 
-//FieldInfo type 
+//FieldInfo type
 type FieldInfo struct {
-	Symbol string `json:symbol`
-	Series string `json:series`
-	OpenPrice string `json:openPrice`
-	HighPrice string `json:highPrice`
-	LowPrice string `json:lowPrice`
-	Ltp string `json:ltp`
-	PreviousPrice string `json:previousPrice`
-	NetPrice string `json:netPrice`
-	TradedQuantity string `json:tradedQuantity`
-	TurnoverInLakhs string `json:turnoverInLakhs`
+	Symbol                   string `json:symbol`
+	Series                   string `json:series`
+	OpenPrice                string `json:openPrice`
+	HighPrice                string `json:highPrice`
+	LowPrice                 string `json:lowPrice`
+	Ltp                      string `json:ltp`
+	PreviousPrice            string `json:previousPrice`
+	NetPrice                 string `json:netPrice`
+	TradedQuantity           string `json:tradedQuantity`
+	TurnoverInLakhs          string `json:turnoverInLakhs`
 	LastCorpAnnouncementDate string `json:lastCorpAnnouncementDate`
-	LastCorpAnnouncement string `json:lastCorpAnnouncement`
+	LastCorpAnnouncement     string `json:lastCorpAnnouncement`
 }
 
-
 //Helper to build HTML page string
-func make_html(d *Response) string {
-	
+func makeHTML(d *Response) string {
+
 	//Initial stuff
 	initial := `<!Doctype html>
 <html>
 <head>
 <title>SIMPLE GO TASK</title>
 </head>
-<body>` + `<div align="center">Snapshot Time: `+ d.Time + "</br></br></br>"
-
+<body>` + `<div align="center">Snapshot Time: ` + d.Time + "</br></br></br>"
 
 	//Initial table stuff
 	table := `<table "width:100%">
@@ -67,11 +64,11 @@ func make_html(d *Response) string {
 	<th>TurnoverInLakhs</th>
 	<th>LastCorpAnnouncementDate</th>
 	<th>LastCorpAnnouncement</th></tr>`
-	
+
 	var row string
 	//Iterate over our Data and make appropriate rows for the table
-	for _,v:= range d.Data {
-	row_helper := `
+	for _, v := range d.Data {
+		row_helper := `
 	<tr>
 	<td>%s</td>
 	<td>%s</td>
@@ -84,116 +81,104 @@ func make_html(d *Response) string {
 	<td>%s</td>
 	<td>%s</td>
 	<td>%s</td>
-	<td>%s</td></tr>`	
-	
-	row = row + "\n" + fmt.Sprintf(row_helper, v.Symbol,v.Series,v.OpenPrice, v.HighPrice, v.LowPrice, v.Ltp, v.PreviousPrice, v.NetPrice, v.TradedQuantity,v.TurnoverInLakhs, v.LastCorpAnnouncementDate, v.LastCorpAnnouncement)
+	<td>%s</td></tr>`
+
+		row = row + "\n" + fmt.Sprintf(row_helper, v.Symbol, v.Series, v.OpenPrice, v.HighPrice, v.LowPrice, v.Ltp, v.PreviousPrice, v.NetPrice, v.TradedQuantity, v.TurnoverInLakhs, v.LastCorpAnnouncementDate, v.LastCorpAnnouncement)
 	}
-	
+
 	//Close table
 	row = row + "</table>"
-	
+
 	//Final stuff
 	final := "</body>\n</html>"
-	
+
 	return initial + table + row + final
 }
 
-
-//get_stuff() handles the crawling. Returns type Response which has our data
-func get_stuff(d *Response) {
+//getStuff() handles the crawling. Returns type Response which has our data
+func getStuff(d *Response) {
 
 	//get a Client type
 	scraper := &http.Client{}
-	
+
 	//Build a request
 	request, err := http.NewRequest("GET", "https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json", nil)
-	
+
 	//Some Headers
-	request.Header.Add("User-Agent","Mozilla/5.0(X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0")
+	request.Header.Add("User-Agent", "Mozilla/5.0(X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0")
 	request.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-	
+
 	//Make the request
 	response, err := scraper.Do(request)
-	
+
 	if err != nil {
 		panic(err)
 	}
-	
+
 	defer response.Body.Close()
-	
+
 	//Parse into byte array
-	data,err := ioutil.ReadAll(response.Body)
-	if err!= nil {
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
 		panic(err)
 	}
-	
+
 	//Parse into our struct Response
 	err = json.Unmarshal(data, d)
-	
+
 	//if maintaining snapshot list then uncomment and change function definition
 	//list.Responses = append(list.Responses, *d)
 }
 
-//Simple handler for displaying stuff	
+//Simple handler for displaying stuff
 func handler(w http.ResponseWriter, r *http.Request, stuff string) {
-	fmt.Fprintf(w, fmt.Sprintf("%s",stuff), r.URL.Path[1:])
+	fmt.Fprintf(w, fmt.Sprintf("%s", stuff), r.URL.Path[1:])
 }
 
-
-	
-	
 func main() {
 
 	//ticker returns a type which has a channel with ticks for events after every d duration
 	ticker := time.NewTicker(300 * time.Second) //300 seconds =  5mins
-	quit := make(chan struct{}) //channel of struct{} to bail out of the ticker. Not necessary for perpetually running service
-	
-	//response_list := new(ResponseList)	
+	quit := make(chan struct{})                 //channel of struct{} to bail out of the ticker. Not necessary for perpetually running service
+
+	//response_list := new(ResponseList)
 	d := new(Response)
 
-	//Routine that triggers a crawl every 5 minutes. ticker returns a channel ticker.C for event every duration d
-	go func () {
-		
+	//Routine that triggers a crawl every 5 minutes. ticker returns an event on channel ticker.C every duration d
+	go func() {
+
 		for {
 			select {
-				case <- ticker.C :
-					//spawns another routine to accomodate for quick ticks. Keeping it generic
-					go get_stuff(d)
-				
-				case <- quit:
-					ticker.Stop()
+			case <-ticker.C:
+				//spawns another routine to accomodate for quick ticks. Keeping it generic
+				go getStuff(d)
+
+			case <-quit:
+				ticker.Stop()
 			}
 		}
 	}()
 
 	//catch /snap URl and show current snapshot
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-							stuff:= make_html(d)
-							handler(w,r,stuff)
-							})
-	
-	
+		stuff := makeHTML(d)
+		handler(w, r, stuff)
+	})
+
 	//Maintains a list of all snapshots since program starts.
 	//http.HandleFunc("/snapshots", func(w http.ResponseWriter, r *http.Request){
-								
-								//output, err := json.Marshal(response_list)
-								//fmt.Println(response_list)
-								//if err != nil {
-									//panic(err)
-								//}
-								//handler(w,r,string(output))
-	
-	
-					//})
+
+	//output, err := json.Marshal(response_list)
+	//fmt.Println(response_list)
+	//if err != nil {
+	//panic(err)
+	//}
+	//handler(w,r,string(output))
+
+	//})
 
 	//serve
 	fmt.Println("Starting server on...", os.Getenv("PORT"))
-	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
-	
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
 }
-
-	
-	
-	
-
-
